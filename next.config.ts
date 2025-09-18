b39407696060020b3a6e045c449398adf5743185
Lazaro Alonso <lazarus.alon@gmail.com>
2025-09-18 14:35:09 +0200

    asset

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export' as const,
   trailingSlash: true,
-  basePath: process.env.NODE_ENV === 'production' ? '' : '',
+  basePath: process.env.NODE_ENV === 'production' ? process.env.BASE_PATH || '' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? process.env.BASE_PATH || '' : '',
   images: {
     unoptimized: true,
   },
