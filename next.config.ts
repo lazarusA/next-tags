f58bd04469061f9f725bd31796085ba9912ed9a2
Lazaro Alonso <lazarus.alon@gmail.com>
2025-09-18 14:14:16 +0200

    ci main

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export' as const,
+  trailingSlash: true,
+  basePath: process.env.NODE_ENV === 'production' ? process.env.BASE_PATH || '' : '',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
