35fa4508e4564059e687d2d257fb95ddfe5404a8
Lazaro Alonso <lazarus.alon@gmail.com>
2025-09-18 14:24:30 +0200

    no base?

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export' as const,
   trailingSlash: true,
-  basePath: process.env.NODE_ENV === 'production' ? process.env.BASE_PATH || '' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '' : '',
   images: {
     unoptimized: true,
   },
