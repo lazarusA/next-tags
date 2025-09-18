37141d6f5275a767b036139789d5ded7c957f702
Lazaro Alonso <lazarus.alon@gmail.com>
2025-09-18 15:01:39 +0200

    relative

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
   basePath: basePath,
-  // assetPrefix: basePath ? `${basePath}/` : '',
+  assetPrefix: isProd ? './' : '',
   images: {
     unoptimized: true,
   },
