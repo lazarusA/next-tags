4bfe8132b67462885b050ba9a21ee4d943ede66d
lazarusA <lazarus.alon@gmail.com>
2025-11-27 08:36:29 +0000

    Deploying to gh-pages from @ lazarusA/next-tags@2448ba62bb64d639cf0be2a34fd36b9bae71a72b 🚀

---
+++ latest/_next/static/chunks/136b77d4f5a962ce.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/5a6c3db863a354ce.js",
+  "static/chunks/61af2a0baa937c8c.js",
+  "static/chunks/turbopack-189597d6966e63ee.js"
+])
---
+++ latest/_next/static/chunks/628b5ab4375aebf2.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/6b297c6bc770653d.js",
+  "static/chunks/61af2a0baa937c8c.js",
+  "static/chunks/turbopack-f49ac4fd5e8ad445.js"
+])
